b6e5d5176642672a0f2d3d5a8a1b59d744f92cf8
jijinlian <441205145@qq.com>
2022-06-15 15:15:19 +0800

    zhihu

--- login/zhihu/cookies.py
+++ login/zhihu/cookies.py
@@ -1,36 +1,30 @@
-import aiohttp
-import asyncio
 import time
 
-from cookiespool.config import PROXY_POOL_URL, TEST_URL_MAP
+import requests
+
+from cookiespool.config import TEST_URL_MAP
 from cookiespool.libs import get_user_agent
 
 
-async def get_zhihu_cookie():
-    headers = {
-        "referer": "https://www.zhihu.com/search?type=content&q=python&utm_content=search_preset",
-        "user-agent": get_user_agent(),
+def get_zhihu_cookie(username, password):
+    result = {
+        'status': 3
     }
-    async with aiohttp.ClientSession() as session:
-        # proxy = await get_random_proxy()
-        # async with session.post(TEST_URL_MAP.get('zhihu'), headers=headers,proxy=proxy) as res:
-        async with session.post(TEST_URL_MAP.get('zhihu'), headers=headers) as res:
-            text = await res.text(encoding='utf-8')
-            stmp = int(time.time())
-            dc0 = f'd_c0="{text}=|{stmp}";'
-            print(dc0)
-            return dc0
-
-
-async def get_random_proxy():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(PROXY_POOL_URL) as res:
-            text = await res.text(encoding='utf-8')
-            return {
-                'http': text.strip()
-            }
+    try:
+        headers = {
+            "referer": "https://www.zhihu.com/search?type=content&q=python&utm_content=search_preset",
+            "user-agent": get_user_agent(),
+        }
+        res = requests.post(TEST_URL_MAP.get('zhihu'), headers=headers)
+        if res.status_code == 200:
+            result['status'] = 1
+            result['content'] = f'"{res.text}|{int(time.time())}";'
+    except Exception as e:
+        print('got exception -> {}'.format(e), flush=True)
+    finally:
+        print('Successful to get cookies', result)
+        return result
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_zhihu_cookie())
+    get_zhihu_cookie(username='test', password='test')
